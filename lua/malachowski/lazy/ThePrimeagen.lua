9617ef1e688a602c751152a37bca63684bb0cd78
KacperMalachowski <kacpermaachowski@gmail.com>
2026-03-11 16:39:18 +0100

    automated dev commit

--- lua/malachowski/lazy/ThePrimeagen.lua
+++ lua/malachowski/lazy/ThePrimeagen.lua
@@ -67,11 +67,17 @@ return {
                     source = "cmp"
                 },
             })
+            vim.keymap.set("n", "<leader>9v", function ()
+                _99.vibe()
+            end)
             vim.keymap.set("v", "<leader>9v", function ()
                 _99.visual()
             end)
             vim.keymap.set("n", "<leader>9s", function ()
                 _99.search()
+            end)
+            vim.keymap.set("n", "<leader>9o", function ()
+                _99.open()
             end)
             vim.keymap.set("n", "<leader>9x", function ()
                 _99.stop_all_requests()
